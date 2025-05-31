49380c3cceb22a728089883e5998fb4132b1eac2
Miri Radunsky <miriradunsky@gmail.com>
2025-05-31 23:36:45 +0300

    jwt in webApi

---
+++ WebApi/Services/PasswordService.cs
@@ -0,0 +1,17 @@
+﻿using BCrypt.Net;
+
+namespace WebAPI.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.HashPassword(password, BCrypt.GenerateSalt());
+        }
+
+        public bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Verify(password, hash);
+        }
+    }
+}
