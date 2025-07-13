9f0fce236db1f07c20792fe98c076f0cffcd780d
Ruchama Coenn <r0534104273@gmail.com>
2025-07-13 04:41:24 +0300

    9

--- server/DAL/API/IBranchManagement.cs
+++ server/DAL/API/IBranchManagement.cs
@@ -10,15 +10,10 @@ namespace DAL.API
     public interface IBranchManagement
     {
         Task<bool> UpdateBranchDetails(Branch branch);
-
         Task<bool> DeleteBranch(int branchId);
-
         Task AddBranch(Branch branch);
-
         Task<List<Branch>> GetBranchesByCityID(int cityID);
-
         Task<List<Branch>> GetBranchesByServiceProviderKey(int doctorKey);
-
         Task<List<Branch>> GetAllBranches();
     }
 }
